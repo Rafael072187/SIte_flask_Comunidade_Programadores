6a4d6f51d74758772697a02589c3b1643cdf4cc7
Rafael072187 <rafaelbettencourtr89@gmail.com>
2025-09-15 09:36:42 -0300

    Update __init__.py

--- ComunidadeProgramadores/__init__.py
+++ ComunidadeProgramadores/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from ComunidadeProgramadores.models import Usuario
+from ComunidadeProgramadores import app, database
 
 
 app = Flask(__name__)
@@ -18,6 +20,16 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert alert-info'
+
+engine = SQLAlchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = SQLAlchemy.inspect(engine)
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("Banco de dados criado com sucesso em ComunidadeImpressionadora/comunidade.db")
+else:
+    print("Base de dados já existente")
 
 
 
